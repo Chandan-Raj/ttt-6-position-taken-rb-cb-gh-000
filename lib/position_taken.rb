eb20037f3ce274579c577f3dedf79b6e8626ee0b
Chandan Raj <chandanraj1919@gmail.com>
2019-08-19 23:32:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board,index=0)
-  if(board==[" ", " ", " ", " ", " ", " ", " ", " ", " "] || board = ["", " ", " ", " ", " ", " ", " ", " ", " "] || board = [nil, " ", " ", " ", " ", " ", " ", " ", " "])
+  if(board==[" ", " ", " ", " ", " ", " ", " ", " ", " "] || board = ["", " ", " ", " ", " ", " ", " ", " ", " "] )
     return false
   end
 end
