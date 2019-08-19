44c57ad06b09a77f26a7a4079bb7321a0006e516
Chandan Raj <chandanraj1919@gmail.com>
2019-08-19 23:33:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,4 +4,6 @@ def position_taken?(board,index=0)
   if(board==[" ", " ", " ", " ", " ", " ", " ", " ", " "] || board = ["", " ", " ", " ", " ", " ", " ", " ", " "] )
     return false
   end
+elsif cboard = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+  
 end
